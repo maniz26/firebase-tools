f0a2452040987db845838db69c3126b2eb748c23
djanogly <45178753+djanogly@users.noreply.github.com>
2022-05-31 17:12:54 +0100

    Diable creation of firebase.json & .firebaserc

--- src/commands/init.js
+++ src/commands/init.js
@@ -200,16 +200,16 @@ module.exports = new Command("init [feature]")
         return init(setup, config, options);
       })
       .then(function () {
-        logger.info();
-        utils.logBullet("Writing configuration info to " + clc.bold("firebase.json") + "...");
-        config.writeProjectFile("firebase.json", setup.config);
-        utils.logBullet("Writing project information to " + clc.bold(".firebaserc") + "...");
-        config.writeProjectFile(".firebaserc", setup.rcfile);
-        if (!fsutils.fileExistsSync(config.path(".gitignore"))) {
-          utils.logBullet("Writing gitignore file to " + clc.bold(".gitignore") + "...");
-          config.writeProjectFile(".gitignore", GITIGNORE_TEMPLATE);
-        }
-        logger.info();
+        // logger.info();
+        // utils.logBullet("Writing configuration info to " + clc.bold("firebase.json") + "...");
+        // config.writeProjectFile("firebase.json", setup.config);
+        // utils.logBullet("Writing project information to " + clc.bold(".firebaserc") + "...");
+        // config.writeProjectFile(".firebaserc", setup.rcfile);
+        // if (!fsutils.fileExistsSync(config.path(".gitignore"))) {
+        //   utils.logBullet("Writing gitignore file to " + clc.bold(".gitignore") + "...");
+        //   config.writeProjectFile(".gitignore", GITIGNORE_TEMPLATE);
+        // }
+        // logger.info();
         utils.logSuccess("Firebase initialization complete!");
       });
   });
